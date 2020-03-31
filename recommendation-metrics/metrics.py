a8e5f8477bcc97ce804a8b17473ab5c2f4ee3cfe
Ashok <ashokrayal27@gmail.com>
2020-03-31 23:27:28 +0530

    apk mapk global precision global recall metrics added

--- recommendation-metrics/metrics.py
+++ recommendation-metrics/metrics.py
@@ -1,4 +1,6 @@
-def precision(actual, predicted, k = 5 ):
+import numpy as np
+
+def precision(actual, predicted, k = 5):
     """ Computes precision for recommendation systems using actual items and predicated items at k
 
         If the argument `k` isn't passed in, the default k 5 is used.
@@ -24,7 +26,7 @@ def precision(actual, predicted, k = 5 ):
     result = len(act_set & pred_set) / float(k)
     return result
 
-def recall(actual, predicted, k=5):
+def recall(actual, predicted, k = 5):
     """ Computes recall for recommendation systems using actual items and predicated items at k
 
         If the argument `k` isn't passed in, the default k 5 is used.
@@ -50,6 +52,121 @@ def recall(actual, predicted, k=5):
     pred_set = set(predicted[:k])
     result = len(act_set & pred_set) / float(len(act_set))
     return result
+
+
+def apk(actual, predicted, k = 5):
+    """ Computes average precision for recommendation systems using actual items and predicated items at k
+
+        If the argument `k` isn't passed in, the default k 5 is used.
+
+        Parameters
+        ----------
+        actual : list
+            actual items used/brought by user
+
+        predicted : list
+            items recommended by system
+
+        k : int
+            The items number at which precision is calculated (default is 5)
+        
+        Returns
+        ----------
+        score : double
+            the average precision over actual items and recommended items
+    """
+    
+    # return 0 is no item in actuals list
+    if not actual:
+        return 0.0
+
+    if len(predicted)>k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    return score / min(len(actual), k)
+
+def mapk(actual, predicted, k = 5):
+    """ Computes mean average precision for recommendation systems using actual items and predicated items at k
+
+        If the argument `k` isn't passed in, the default k 5 is used.
+
+        Parameters
+        ----------
+        actual : list
+                A list of lists of elements that are to be predicted 
+                (order doesn't matter in the lists)
+
+        predicted : list
+            A list of lists of predicted elements
+                (order matters in the lists)
+
+        k : int
+            The items number at which mean average precision is calculated (default is 5)
+        
+        Returns
+        ----------
+        score : double
+            the mean average precision over actual items and recommended items
+    """
+    return np.mean([apk(a,p,k) for a,p in zip(actual, predicted)])
+
+def global_precision(actual, predicted, k = 5):
+    """ Computes global precision for recommendation systems using actual items and predicated items at k
+
+        If the argument `k` isn't passed in, the default k 5 is used.
+
+        Parameters
+        ----------
+        actual : list
+                A list of lists of elements that are to be predicted 
+                (order doesn't matter in the lists)
+
+        predicted : list
+            A list of lists of predicted elements
+                (order matters in the lists)
+
+        k : int
+            The items number at which mean average precision is calculated (default is 5)
+        
+        Returns
+        ----------
+        score : double
+            the global precision over actual items and recommended items
+    """
+    return np.mean([precision(a,p,k) for a,p in zip(actual, predicted)])
+
+def global_recall(actual, predicted, k = 5):
+    """ Computes global recall for recommendation systems using actual items and predicated items at k
+
+        If the argument `k` isn't passed in, the default k 5 is used.
+
+        Parameters
+        ----------
+        actual : list
+                A list of lists of elements that are to be predicted 
+                (order doesn't matter in the lists)
+
+        predicted : list
+            A list of lists of predicted elements
+                (order matters in the lists)
+
+        k : int
+            The items number at which global recall is calculated (default is 5)
+        
+        Returns
+        ----------
+        score : double
+            the global recall over actual items and recommended items
+    """
+    return np.mean([recall(a,p,k) for a,p in zip(actual, predicted)])
 
 if __name__ == "__main__":
     actuals = [1,2,3]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'recommendation-metrics',         # How you named your package folder (MyLib)
   packages = ['recommendation-metrics'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Metrics for recommendation system',   # Give a short description about your library
   author = 'Ashok Rayal',                   # Type in your name
@@ -10,10 +10,10 @@ setup(
   url = 'https://github.com/Ashokrayal',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/Ashokrayal/Recommendation-Metrics/archive/V0.1.tar.gz',    # I explain this later on
   keywords = ['Recommendaton System', 'Machine Learning', 'Meterics'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  install_requires=[ 'numpy'          # I get to this in a second
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
